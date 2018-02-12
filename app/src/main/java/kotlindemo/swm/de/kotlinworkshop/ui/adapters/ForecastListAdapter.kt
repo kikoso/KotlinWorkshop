df479158ad198c7e80f43f1417bbd02828f87dd9
Enrique Lopez Manas <eenriquelopez@gmail.com>
2018-02-12 17:35:19 +0900

    Updated Gradle Version

--- app/src/main/java/kotlindemo/swm/de/kotlinworkshop/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/kotlindemo/swm/de/kotlinworkshop/ui/adapters/ForecastListAdapter.kt
@@ -1,14 +1,8 @@
 package kotlindemo.swm.de.kotlinworkshop.ui.adapters
 
-import android.support.v7.widget.AppCompatAutoCompleteTextView
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
-import org.w3c.dom.Text
-
-/**
- * Created by enriquelopezmanas on 01.02.18.
- */
 
 class ForecastListAdapter(val items : List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
     override fun getItemCount() = items.size
